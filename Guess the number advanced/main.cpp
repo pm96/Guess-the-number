da789eca7dfe2bc2db5c2aa355c995b82b4dde53
Pål Magnus <pm96@live.no>
2019-01-14 12:39:27 +0100

    Initial Commit

---
+++ Guess the number advanced/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Guess the number advanced
+//
+//  Created by Pål Magnus Østern on 14/01/2019.
+//  Copyright © 2019 Pål Magnus Østern. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
